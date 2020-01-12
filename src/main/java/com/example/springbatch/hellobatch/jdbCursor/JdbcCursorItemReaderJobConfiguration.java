ea57a7c747afcc90343404067df060ca6bdc70ad
cchoimin91 <cchoimin1991@gmail.com>
2020-01-13 07:39:47 +0900

    cusor 특징 및 주의사항 주석 추가

--- src/main/java/com/example/springbatch/hellobatch/jdbCursor/JdbcCursorItemReaderJobConfiguration.java
+++ src/main/java/com/example/springbatch/hellobatch/jdbCursor/JdbcCursorItemReaderJobConfiguration.java
@@ -19,16 +19,18 @@ import javax.sql.DataSource;
 /**
  * JDBC Cursor 특징
  * - Streaming으로 데이터 처리
- *
- * ItemReader의 장점
  * - 데이터를 striming
  * - reader, processor , writer가 Chunk단위로 수행되고 주기적으로 commit됨 이는 고성능 배치처리의 핵심
+ * - JdbcPagingItemReader에 비해 더 많은 메모리를 사용하고 더 빠름
+ * - 대량데이터가 아니고, 멀티스레드 환경이 아닌 곳에서는 매우 적합
  *
  * CursorItemReader 주의사항
  * - db와 socketTimeOut을 충분히 큰 값으로 설정해야 함
  * - 왜? cursor는 하나의 connection을 사용하기 때문
- * - batch수행이 오래 걸리는 경우 PagingItemReader를 사용하는게 나음
+ * - batch수행이 오래 걸리는 경우 PagingItemReader를 사용하는게 나음 (Cursor는 한번에 가져오는 데이터 양이 너무 많으면 batch 뻗음)
  * - 왜? Paging의 경우 한페이지를 읽을때 마다 connection을 맺고 끊음. 아무리 많은 데이터라도 timeout과 부하없이 수행 가능
+ *
+ *
  */
 @Slf4j
 @RequiredArgsConstructor
@@ -60,7 +62,7 @@ public class JdbcCursorItemReaderJobConfiguration {
     //READER
     @Bean
     public JdbcCursorItemReader<Pay> jdbcCursorItemReader() {
-        log.info(">>>>> READER START ");
+        log.info(">>>>> JdbcCursorItemReader START ");
         return new JdbcCursorItemReaderBuilder<Pay>()
                 .fetchSize(chunkSize) // DB에서 한번에 가져올 데이터양
                 .dataSource(dataSource)
@@ -72,7 +74,7 @@ public class JdbcCursorItemReaderJobConfiguration {
 
     //WRITER
     private ItemWriter<Pay> jdbcCursorItemWriter() {
-        log.info(">>>>> WRITER START !!!");
+        log.info(">>>>> jdbcCursorItemWriter START !!!");
         return list -> {
             for (Pay pay: list) {
                 log.info("Current Pay={}", pay);
