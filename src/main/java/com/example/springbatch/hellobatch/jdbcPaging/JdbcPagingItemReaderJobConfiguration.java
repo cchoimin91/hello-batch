872e875f895788e87cb6107569504f593251eb9c
cchoimin91 <cchoimin1991@gmail.com>
2020-01-13 07:40:24 +0900

    paging 주석 추가

---
+++ src/main/java/com/example/springbatch/hellobatch/jdbcPaging/JdbcPagingItemReaderJobConfiguration.java
@@ -0,0 +1,106 @@
+package com.example.springbatch.hellobatch.jdbcPaging;
+
+import com.example.springbatch.hellobatch.model.Pay;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * JdbcPagingItemReader
+ *
+ * springBatch에서는 offset,limit을 pageSize에 맞게 자동으로 생성해줌
+ * 각 쿼리는 개별적으로 실행한다는 점에 유의
+ * 페이징시 결과를 정렬하는 것이 중요함
+ * 데이터 결과의 순서가 보장될 수 있도록 order by가 권장됨됨
+ *
+ * JdbcCursorItemReader에 비해 메모리를 적게 사용하고, 속도가 비교적 느림
+ * */
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class JdbcPagingItemReaderJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource; // DataSource DI
+
+    private static final int chunkSize = 2;
+
+    @Bean
+    public Job jdbcPagingItemReaderJob() throws Exception {
+        return jobBuilderFactory.get("jdbcPagingItemReaderJob")
+                .start(jdbcPagingItemReaderStep())
+                .build();
+    }
+
+    @Bean
+    public Step jdbcPagingItemReaderStep() throws Exception {
+        return stepBuilderFactory.get("jdbcPagingItemReaderStep")
+                .<Pay, Pay>chunk(chunkSize)
+                .reader(jdbcPagingItemReader())
+                .writer(jdbcPagingItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JdbcPagingItemReader<Pay> jdbcPagingItemReader() throws Exception {
+        log.info(">>>>> JdbcPagingItemReader START !!!");
+        Map<String, Object> parameterValues = new HashMap<>();
+        parameterValues.put("amount", 1);
+
+        return new JdbcPagingItemReaderBuilder<Pay>()
+                .pageSize(chunkSize)
+                .fetchSize(chunkSize) // DB에서 한번에 가져올 데이터 사이즈
+                .dataSource(dataSource)
+                .rowMapper(new BeanPropertyRowMapper<>(Pay.class))
+                .queryProvider(createQueryProvider())
+                .parameterValues(parameterValues)
+                .name("jdbcPagingItemReader")
+                .build();
+    }
+
+    private ItemWriter<Pay> jdbcPagingItemWriter() {
+        log.info(">>>>> JdbcPagingItemWRITER START !!!");
+        return list -> {
+            for (Pay pay: list) {
+                log.info("Current Pay={}", pay);
+            }
+        };
+    }
+
+    /**
+     * JdbcCusorItemReader와 다른 점
+     * 각 DB에는 paging을 지원하는 자체적략들이 있음.
+     */
+    @Bean
+    public PagingQueryProvider createQueryProvider() throws Exception {
+        SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
+        queryProvider.setDataSource(dataSource); // Database에 맞는 PagingQueryProvider를 선택하기 위해
+        queryProvider.setSelectClause("id, amount, txName, txDateTime");
+        queryProvider.setFromClause("from pay");
+        queryProvider.setWhereClause("where amount >= :amount"); // parameterValues의 key값과 동일해야함
+
+        Map<String, Order> sortKeys = new HashMap<>(1);
+        sortKeys.put("id", Order.ASCENDING);
+
+        queryProvider.setSortKeys(sortKeys);
+
+        return queryProvider.getObject();
+    }
+}
